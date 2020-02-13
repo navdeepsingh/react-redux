9274925f1d98f55a87cc348d2b4104bef2ea54b8
Navdeep Singh <navdeep.er@gmail.com>
2020-02-13 21:39:27 +0530

    update

--- src/reducer.js
+++ src/reducer.js
@@ -1,3 +1,4 @@
+import { combineReducers } from 'redux'
 import {
   ADD_TODO,
   TOGGLE_TODO,
@@ -5,10 +6,8 @@ import {
   VisibilityFilters
 } from './actions'
 
-const initialState = {
-  visibilityFilter: VisibilityFilters.SHOW_ALL,
-  todos: []
-}
+const { SHOW_ALL } = VisibilityFilters
+
 
 function todos(state = [], action) {
   switch (action.type) {
@@ -32,25 +31,31 @@ function todos(state = [], action) {
     default:
       return state
   }
+}
 
-  function todoApp(state = initialState, action) {
-    switch (action.type) {
-      case ADD_TODO:
-        return Object.assign({}, state, {
-          todos: todos(state.todos, action)
-        })
-      case TOGGLE_TODO:
-        return Object.assign({}, state, {
-          todos: todos(state.todos, action)
-        })
-      case SET_VISIBILITY_FILTER:
-        // Object.assign({}, state, {visibilityFilter: action.filter})
-        return { ...state, visibilityFilter: action.filter }
-      default:
-        return state;
-    }
-
+function visibilityFilter(state = SHOW_ALL, action) {
+  switch (action.type) {
+    case SET_VISIBILITY_FILTER:
+      return action.filter
+    default:
+      return state
   }
 }
+
+// function todoApp(state = {}, action) {
+//   return {
+//     visibilityFilter: visibilityFilter(state.visibilityFilter, action),
+//     todos: todos(state.todos, action)
+//   }
+// }
+
+const todoApp = combineReducers(
+  visibilityFilter,
+  todos
+)
+
+export default todoApp
+
+
 
 
